06b37b8eb4204c436fe3cf48dccd6abfbadd846c
Tom Cox <tcox56_98@yahoo.com>
2012-02-12 10:13:50 -0600

    heroku_txf wasnt wiping out entire db.  hacked to remove-replace addon to achieve clean db.

--- heroku_txf.sh
+++ heroku_txf.sh
@@ -14,5 +14,12 @@ if [ "$1" == "" -o   "$1" == "-h" -o "$2" == "" ]; then
     exit 0
 fi
 
+heroku maintenance:on --app $TARGET_ENV
+
+heroku addons:remove shared-database --app $TARGET_ENV --confirm $TARGET_ENV
+heroku addons:add shared-database:5mb --app $TARGET_ENV
+
 heroku pgbackups:capture --app $SRC_ENV
-heroku pgbackups:restore DATABASE `heroku pgbackups:url --app $SRC_ENV` --app $TARGET_ENV
+heroku pgbackups:restore DATABASE `heroku pgbackups:url --app $SRC_ENV` --app $TARGET_ENV --confirm $TARGET_ENV
+
+heroku maintenance:off --app $TARGET_ENV
