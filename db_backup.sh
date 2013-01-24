f1cd2d822a9da2d9ac747e678fb1d0177c29773c
tcox5698 <tcox56_98@yahoo.com>
2013-01-24 12:09:52 -0600

    took out hardcoded user

--- db_backup.sh
+++ db_backup.sh
@@ -17,6 +17,6 @@ if [ -f $DBWORK_DIR/$TARGET_DB.bak ] ; then
   mv $DBWORK_DIR/$TARGET_DB.bak $DBWORK_DIR/x$TARGET_DB.bak.$CUR_DATE
 fi
 
-pg_dump -U postgres -f $DBWORK_DIR/$TARGET_DB.bak $TARGET_DB
+pg_dump -f $DBWORK_DIR/$TARGET_DB.bak $TARGET_DB
 
 echo "$TARGET_DB has been backed up - BUT YOU BETTER VERIFY CAUSE NON-EXISTENT DBs BACKUP SUCCESSFULLY"
